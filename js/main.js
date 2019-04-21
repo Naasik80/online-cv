036afa1a5dd1ae5096332917d9d2df4a7999b326
Naasik kamish <nrkamish@gmail.com>
2019-04-21 16:08:39 +0200

    create basic preload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 // first preload //
+$(document).ready(function() {
+	// Users can skip the loading process if they want.
+	$('.skip').click(function() {
+		$('.overlay, body').addClass('loaded');
+	})
+	
+})
 
+
+    function sidebar(){
+        document.getElementById("tim").classList.toggle('active');
+    }
+      
 // first preload //
 
